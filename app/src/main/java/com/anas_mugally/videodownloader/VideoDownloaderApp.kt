f13366e5467a7369f3540e23b2821ff933e628f0
anas mugally <58054553+AnasMually@users.noreply.github.com>
2026-08-17 16:43:30 +0300

    Wire Firebase custom ads repository into app

--- app/src/main/java/com/anas_mugally/videodownloader/VideoDownloaderApp.kt
+++ app/src/main/java/com/anas_mugally/videodownloader/VideoDownloaderApp.kt
@@ -2,6 +2,7 @@ package com.anas_mugally.videodownloader
 
 import android.app.Application
 import com.anas_mugally.videodownloader.data.AppRepository
+import com.anas_mugally.videodownloader.data.MyAppAdsRepository
 import com.anas_mugally.videodownloader.data.VideoFlowApi
 import com.anas_mugally.videodownloader.download.DownloadNotifications
 import kotlinx.coroutines.CoroutineScope
@@ -12,6 +13,7 @@ import kotlinx.coroutines.launch
 class VideoDownloaderApp : Application() {
     val repository by lazy { AppRepository(this) }
     val api by lazy { VideoFlowApi() }
+    val adsRepository by lazy { MyAppAdsRepository(this) }
     private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     override fun onCreate() {
