84d6ea26619c041dcc68ea6d04dfded41b0c377b
anas mugally <58054553+AnasMually@users.noreply.github.com>
2026-08-17 05:28:15 +0300

    Use remote VideoFlow API at application level

--- app/src/main/java/com/anas_mugally/videodownloader/VideoDownloaderApp.kt
+++ app/src/main/java/com/anas_mugally/videodownloader/VideoDownloaderApp.kt
@@ -1,9 +1,8 @@
 package com.anas_mugally.videodownloader
 
 import android.app.Application
-import android.util.Log
 import com.anas_mugally.videodownloader.data.AppRepository
-import com.anas_mugally.videodownloader.data.YtDlpRuntime
+import com.anas_mugally.videodownloader.data.VideoFlowApi
 import com.anas_mugally.videodownloader.download.DownloadNotifications
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,19 +11,12 @@ import kotlinx.coroutines.launch
 
 class VideoDownloaderApp : Application() {
     val repository by lazy { AppRepository(this) }
-    val ytDlpRuntime by lazy { YtDlpRuntime(this) }
+    val api by lazy { VideoFlowApi() }
     private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     override fun onCreate() {
         super.onCreate()
         DownloadNotifications.createChannels(this)
-        applicationScope.launch {
-            runCatching { ytDlpRuntime.ensureReady() }
-                .onFailure { error -> Log.e(TAG, "yt-dlp initialization failed", error) }
-        }
-    }
-
-    private companion object {
-        const val TAG = "VideoDownloaderApp"
+        applicationScope.launch { api.refreshHealth() }
     }
 }
